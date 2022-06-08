09f4574cb9684267930c70d94c9e6db8fd2f2049
lijimol-hashroot <92438222+lijimol-hashroot@users.noreply.github.com>
2022-06-08 15:28:02 +0530

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('phone');
+            $table->string('employee_id');
+            $table->json('skill_set')->nullable();
+            $table->string('experience')->nullable();
+            $table->string('turnaround_time')->nullable();
+            $table->string('availability')->nullable();
+            $table->string('rate')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

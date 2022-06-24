4debd8c4e297a87379af801bbe8acc79d31e403a
akhilshashroot <93207850+akhilshashroot@users.noreply.github.com>
2022-06-24 15:45:48 +0530

    popup

---
+++ resources/views/stakefield/contactpopup.blade.php
@@ -0,0 +1,69 @@
+
+  <!-- Modal -->
+  <div class="modal fade" id="mycontact" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content border-0">
+          <div class="view show" id="modal-contact-view">
+            <div class="modal-header border-0 bg-dark px-4">
+              <h4 class="modal-title text-light">GET IN TOUCH</h4>
+              <button class="btn-close btn-close-white" type="button" data-dismiss="modal" aria-label="btn-close"></button>
+            </div>
+            <div class="modal-body px-4">
+              <p class="fs-ms text-muted">Please provide the following details and we will get back to you.</p>
+              <form method="post" id="contactForm" class="waituk_contact-form" novalidate="novalidate" action="#" enctype="multipart/form-data">
+            
+              
+                                           <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <input type="text" placeholder="Name *" id="name1" name="name1" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <input type="email" placeholder="Email *" id="email1" name="email1" class="form-control">
+                                                </div>
+                                            </div>
+
+
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <input type="text" placeholder="Phone" id="phone1" name="phone1" class="form-control">
+                                                </div>
+                                            </div>
+                                           
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <textarea class="form-control cs"  rows="2" cols="200"  id="r1_message" name="r1_message"  placeholder="Message *" required=""></textarea>
+
+                                                
+                                                </div>
+                                            </div>
+
+                                            <!-- <div class="col-md-6 justify-center">
+
+                                            </div> -->
+                                            <!-- <div class="btn-container mx-auto">
+                                                <button id="btn_sent" class="btn btn-primary btn-arrow">SUBMIT</button>
+                                                <p id="error_message"> </p>
+                                            </div> -->
+                                            <div class="btn-container mx-auto pt-5">
+                                            <button class="btn btn-primary d-block w-100" id="submit1" type="submit">Submit</button>
+ </p>
+                                            </div>
+
+                                    
+                                </div></form>
+               
+
+
+              </form>
+            </div>
+          </div>
+
+        </div>
+      
+    </div>
+    </div>
